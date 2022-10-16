f14ca9dd6e1afb28b3b9c7e7c4d7a198276a6eaa
BenBrandon97 <benbrandon97@gmail.com>
2022-10-16 07:46:36 -0700

    Print single digits of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - single digits to base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int s;
+
+	s = 0;
+	while
+		(s < 10)
+		{
+			putchar(s + '0');
+			s++;
+		}
+	putchar('\n');
+	return (0);
+}
