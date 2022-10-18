6fc67fe34e8a4e6da4b1e9b098341904491057d2
BenBrandon97 <benbrandon97@gmail.com>
2022-10-18 03:16:44 -0700

    putchar task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - print _putchar followed by new line
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
