9a693dfed4a5ec39dd3803a870babfcc0b4af185
BenBrandon97 <benbrandon97@gmail.com>
2022-10-21 04:44:24 -0700

    Print FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints FizzBuzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int k;
+
+	printf("l");
+	for (k = 2; k <= 100; k++)
+	{
+		printf(" ");
+		if (k % 3 == 0)
+			printf("Fizz");
+
+		if (k % 5 == 0)
+			printf("Buzz");
+
+		if (k % 3 != 0 && k % 5 != 0)
+			printf("%d", k);
+	}
+	printf("\n");
+
+	return (0);
+}
