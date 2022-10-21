0c3632d408a3626bdc2f651b2ae51c91a23bf7c9
BenBrandon97 <benbrandon97@gmail.com>
2022-10-21 04:25:18 -0700

    Printing a squre using # tag in terminal

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - prints square
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int l1, l2;
+
+		for (l1 = 0; l1 < size; l1++)
+		{
+			for (l2 = 0; l2 < size; l2++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
