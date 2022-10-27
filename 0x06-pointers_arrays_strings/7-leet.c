987d92b3ac0340733c010a8b71fb293a7f3b6751
BenBrandon97 <benbrandon97@gmail.com>
2022-10-27 12:22:22 -0700

    encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
  * leet - encodes a string into 1337
- * @s: string to encode
+ * @n: string to encode
  * Return: Encoded string
  */
-char *leet(char *s)
+char *leet(char *n)
 {
 	int a, b;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (a = 0; s[a] != '\n'; a++)
+	for (a = 0; n[a] != '\n'; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			if (s[a] == s1[b])
+			if (n[a] == s1[b])
 			{
-				s[a] = s2[b];
+				n[a] = s2[b];
 			}
 		}
 	}
-	return (s);
+	return (n);
 }
