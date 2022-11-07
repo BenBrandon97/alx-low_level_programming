f8e7f4eeb2bcaefd104ba8d1b0fbabe116e8cf14
BenBrandon97 <benbrandon97@gmail.com>
2022-11-07 02:42:51 -0800

    Advanced

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - prints minimum couns to make change
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: Always 0
+  */
+int main(int argc, char *argv[])
