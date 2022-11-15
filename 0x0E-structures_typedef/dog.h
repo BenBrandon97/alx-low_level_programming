51c54e88de6ce1b4ac76f105210084adb2776df5
BenBrandon97 <benbrandon97@gmail.com>
2022-11-15 10:25:51 -0800

    Commit for struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+  * struct dog - Attributes of the dog
+  * @name: Name of the dog
+  * @age: Age of the dog
+  * @owner: Owner of the dog
+  */
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+  * dog_t - dog structure typedef
+  */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
