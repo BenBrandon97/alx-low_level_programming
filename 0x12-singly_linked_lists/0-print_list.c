d869b593d5bcac76d671989ca39d1e5aa41f83fb
BenBrandon97 <benbrandon97@gmail.com>
2022-11-28 15:15:27 +0000

    Correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,11 +4,10 @@
 
 /**
   * print_list - Prints all elements of a list
+  * size_t - ...
   * @h: The linked list
-  *
   * Return: The number of nodes
   */
-
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
