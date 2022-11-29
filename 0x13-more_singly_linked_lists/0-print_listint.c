ab90a5bb3ea2c6192bf56f4b0636de3ce983353c
Ben Brandon/ <benbrandon97@gmail.com>
2022-11-29 20:33:40 +0300

    White space remove

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
-  * print_listint - Prints all the elements of a linked list
-  * @h: The head of the linked list
-  * Return: The number of nodes of the linked list
-  */
+ * print_listint - Prints all the elements of a linked list
+ * @h: The head of the linked list
+ * Return: The number of nodes of the linked list
+ */
 
 size_t print_listint(const listint_t *h)
 {
