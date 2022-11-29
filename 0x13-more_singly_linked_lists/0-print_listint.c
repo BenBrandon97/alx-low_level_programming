e7ecc9f609f522459287d1f59524f44437d188a5
Ben Brandon/ <benbrandon97@gmail.com>
2022-11-29 20:13:42 +0300

    Input for 0-print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,9 +4,9 @@
 /**
   * print_listint - Prints all the elements of a linked list
   * @h: The head of the linked list
-  *
   * Return: The number of nodes of the linked list
   */
+
 size_t print_listint(const listint_t *h)
 {
 	int count = 0;
@@ -20,6 +20,5 @@ size_t print_listint(const listint_t *h)
 			count++;
 		}
 	}
-
 	return (count);
 }
